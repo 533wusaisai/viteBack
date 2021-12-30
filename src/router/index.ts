af0bb7ff782aa010468b6f733fb3f662e87f0f4b
wusaisai <53923594+533wusaisai@users.noreply.github.com>
2021-12-30 09:49:39 +0800

    feat: 路由守卫修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
+import NProgress from 'nprogress'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import '../assets/styles/NProgress.less'
 import router from './router'
 const routes: Array<RouteRecordRaw> = router
+NProgress.configure({ showSpinner: false })
 
 const route = createRouter({
   history: createWebHistory(),
   routes,
 })
 route.beforeEach((to, from, next) => {
+  NProgress.start()
+  const title: any = to.meta.title
+  document.title = title
   // 跳转404页面
   if (to.matched.length === 0) {
     // from.name ? next({ name: from.name }) : next('/404')
@@ -19,5 +25,9 @@ route.beforeEach((to, from, next) => {
   //封装请求中  //在跳转路由之前，先清除所有的请求
   // clearPending()
   next()
+})
+
+route.afterEach(() => {
+  NProgress.done()
 })
 export default route
---
+++ src/typing/shims-vue.d.ts
@@ -0,0 +1,4 @@
+// 解决node_modules 找不到模块问题
+
+declare module 'lodash-es'
+declare module 'nprogress'
